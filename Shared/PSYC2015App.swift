44087c4b50cb7f876d1c18073832074f0be1155c
TanmoyPanigrahi <tanmoy.panigrahi2@gmail.com>
2022-03-02 23:19:28 -0500

    Initial Commit

---
+++ Shared/PSYC2015App.swift
@@ -0,0 +1,17 @@
+//
+//  PSYC2015App.swift
+//  Shared
+//
+//  Created by Tanmoy Panigrahi on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PSYC2015App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
